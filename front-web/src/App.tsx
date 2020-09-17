8627af0e25ceea07ea24af732633c40e736be091
Caio Avelar <caioavelar17@hotmail.com>
2020-09-16 22:23:36 -0300

    Criando página home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import Header from './components/Header/index';
-
+import Header from './components/Header';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
